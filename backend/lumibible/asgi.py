149cd0356d38f69c891e6b30096b3bf7ce6775f2
Pierre-Yves Genest <99874577+pygalt@users.noreply.github.com>
2025-11-29 11:46:33 +0100

    feat: enhance project configuration with environment variables and dotenv support

--- backend/lumibible/asgi.py
+++ backend/lumibible/asgi.py
@@ -1,5 +1,4 @@
-"""
-ASGI config for lumibible project.
+"""ASGI config for lumibible project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- backend/lumibible/wsgi.py
+++ backend/lumibible/wsgi.py
@@ -1,5 +1,4 @@
-"""
-WSGI config for lumibible project.
+"""WSGI config for lumibible project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
